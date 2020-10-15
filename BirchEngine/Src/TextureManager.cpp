0f7de73861385eef21ba22e0aa0b676d6f02eaa6
Trispir <72822155+Trispir@users.noreply.github.com>
2020-10-14 22:15:10 -0700

    Initial commit

---
+++ BirchEngine/Src/TextureManager.cpp
@@ -0,0 +1,20 @@
+#include "TextureManager.h"
+#include "SDL.h"
+#include "SDL_image.h"
+
+SDL_Texture* TextureManager::LoadTexture(const char* texture)
+{
+	SDL_Surface* tempSurface = IMG_Load(texture);
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
+	SDL_FreeSurface(tempSurface);
+
+	return tex;
+}
+
+void TextureManager::Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest)
+{
+	SDL_RenderCopy(Game::renderer, tex, &src, &dest);
+}
+
+
+
